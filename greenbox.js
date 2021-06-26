81d314334c90cfdde8d27ff4ec569056dd6b522b
bhoomi-wq <83025141+bhoomi-wq@users.noreply.github.com>
2021-06-26 14:17:10 +0530

    Add files via upload

---
+++ greenbox.js
@@ -0,0 +1,41 @@
+class GreenBox {
+
+    constructor(x, y, width, height) {
+        var options = {
+
+            isStatic: false,
+            restitution: 0.1,
+            frictions: 0.01,
+            density:0.02
+
+        }
+        this.Visiblity = 255;
+        this.body = Bodies.rectangle(x, y, width, height, options);
+
+        //NameSpacing(renaming width and height)
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+    }
+
+    display() {
+     //NameSpacing(renaming position of Ground)
+        var pos = this.body.position;
+        push();
+        if(this.body.speed < 3){
+            strokeWeight(2);
+            fill("green");
+            rect(pos.x, pos.y, this.width, this.height);
+            pop();
+     
+        }
+         else{
+         push();
+         World.remove(world, this.body);
+         this.Visiblity=this.Visiblity-5;
+         tint(255,this.Visiblity);
+         pop();
+        }      
+     }
+
+    }
